b9e28bb95f962880dbe327ba22158c2b8698ff19
cnapolea <cristianojorgeapple0994@Cristianos-MacBook-Pro.local>
2019-09-25 20:50:36 -0500

    Restructuring Code

---
+++ ftw_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/cristianojorgeapple0994/Documents/main_projects/FintalkWorld/ftw_venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
