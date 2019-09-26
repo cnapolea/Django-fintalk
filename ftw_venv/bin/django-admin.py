b3f340e7d631f085ea3cce524cd223d5a9e847d7
cnapolea <cristianojorgeapple0994@Cristianos-MacBook-Pro.local>
2019-09-25 20:57:46 -0500

    Revert "Restructuring Code"
    This reverts commit b9e28bb95f962880dbe327ba22158c2b8698ff19.

--- ftw_venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/cristianojorgeapple0994/Documents/main_projects/FintalkWorld/ftw_venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
